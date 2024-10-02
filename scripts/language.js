f828e8129e531b3e1a1e128a7c52e425250c56d3
at997-student <at997@students.waikato.ac.nz>
2024-10-03 11:48:20 +1300

    starting on outline of description

--- scripts/language.js
+++ scripts/language.js
@@ -1,8 +1,11 @@
 let languages = document.querySelectorAll(".languageName");
 let descriptions = document.getElementById("languageDescriptions")
 let previouseClicked = null;
+let currentEl = null;
+const left = document.querySelector('#rectleft');
+const right = document.querySelector('#rectright');
 
-for (let i = 0; i < descriptions.children.length; i++) {
+for (let i = 1; i < descriptions.children.length; i++) {
     const element = descriptions.children[i];
     element.style.display = "none";
 }
@@ -43,15 +46,56 @@ languages.forEach(element => {
         previouseClicked = inner;
         // inner.classList.add("clickedName")
         let eleColor = element.style.color;
-        console.log(eleColor);
+        currentEl = element;
 
         // inner.style.borderTop = "solid thin " + eleColor;
         // inner.style.borderBottom = "solid thin " + eleColor;
 
-
+        setUpPath();
+        doPath("green")
 
     });
     i++;
 })
 
 
+function setUpPath() {
+	let x = (currentEl.offsetLeft + currentEl.clientWidth / 2);
+
+	x -= 20;
+	let h = window.innerHeight - left.parentElement.getBoundingClientRect().y - 20;
+	let x2 = window.innerWidth - x - 40;
+
+	// Create svg paths
+	left.setAttribute('d', `M ${x + 2},0
+							C 100,0 40,0 40,0
+							40,0 0,0 0,40
+							0,40 0,60 0,${h - 40}
+							0,${h - 40} 0,${h} 40,${h}
+							93.75,${h} 100,${h} ${x2},${h}`);
+	right.setAttribute('d', `M -2,0
+							 C -2,0 ${x2 - 40},0 ${x2 - 40},0
+							 ${x2 - 40},0 ${x2},0 ${x2},40
+							 ${x2},40 ${x2},60 ${x2},${h - 40}
+							 ${x2},${h - 40} ${x2},${h} ${x2 - 40},${h}
+							 ${x2 - x + 40},${h} ${x2 - x},${h} ${x2 - x},${h}`);
+	right.style.transform = `translateX(${x + 1}px) translateY(1px)`;
+	// Reset everything
+	let len = left.getTotalLength();
+	left.style.transition = 'initial';
+	left.style.strokeDasharray = len;
+	right.style.transition = 'initial';
+	right.style.strokeDasharray = len;
+
+}
+
+function doPath(color) {
+	left.style.stroke = color;
+	right.style.stroke = color;
+
+	// Do animation
+	left.style.transition = 'stroke-dashoffset .75s ease-in-out';
+	right.style.transition = 'stroke-dashoffset .75s ease-in-out';
+	left.style.strokeDashoffset = '0';
+	right.style.strokeDashoffset = '0';
+}
