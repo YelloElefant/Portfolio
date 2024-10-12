6c16bebbf36d3611826abf332e7bd49cefa2a637
YelloElefant <alextrotternz@gmail.com>
2024-10-12 22:47:51 +1300

    fix to age
    Signed-off-by: YelloElefant <alextrotternz@gmail.com>

--- scripts/age.js
+++ scripts/age.js
@@ -2,5 +2,5 @@ let ageEl = document.getElementById("age");
 
 setInterval(() => {
    let time = dayjs().diff(dayjs(1117866421000), 'year', true);
-   ageEl.innerText = time.toString().substring(0, 12);
+   ageEl.innerText = "Age: " + (time.toString().substring(0, 12));
 }, 50);
